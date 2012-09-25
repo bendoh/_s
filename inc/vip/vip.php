c7fcd2a7a93cdb432ff913455f5f0fa94a38d5d3
John Cionci <johncionci@gmail.com>
2012-09-25 12:36:39 -0400

    Adding VIP functions

---
+++ inc/vip/vip.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * VIP functions, per http://lobby.vip.wordpress.com/getting-started/development-environment/
+ */
+
+// Init WP.com VIP environment
+require_once( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' );
